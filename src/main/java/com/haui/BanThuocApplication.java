63a0b6a7cd9fa5cebfcadf4f2c05108294ed8414
Minhkz <minhluky0@gmail.com>
2025-12-03 09:45:59 +0700

    first commit

---
+++ src/main/java/com/haui/BanThuocApplication.java
@@ -0,0 +1,13 @@
+package com.haui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanThuocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanThuocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haui/BanThuocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanThuocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
